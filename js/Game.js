14cf33e903c6b7747e148fb52fb54fe4e3149990
John Reed <contact.johnreed@gmail.com>
2020-12-03 18:51:25 -0500

    added Game and Phrase class's

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,17 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+   constructor(){
+     this.missed = 0
+     this.phrases = ['phrase 1', 'phrase 2', 'phrase 3', 'phrase 4', 'phrase 5']
+     this.activePhrase = null
+   }
+
+   // createPhrases(){
+   //   const phrases = ['phrase 1', 'phrase 2', 'phrase 3', 'phrase 4', 'phrase 5']
+   //   return phrases
+   // }
+
+ }
