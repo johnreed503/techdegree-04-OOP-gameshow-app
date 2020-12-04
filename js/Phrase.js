2f99d63d989e4ec34c5068f8ef3ad1529719b878
John Reed <contact.johnreed@gmail.com>
2020-12-04 14:42:04 -0500

    added addPhraseToDisplay method to Phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,4 +6,25 @@
    constructor(phrase){
      this.phrase = phrase.toLowerCase()
    }
+
+  /**
+   * Display phrase on game board
+  */
+  addPhraseToDisplay() {
+    const listArea = document.querySelector('phrase, ul')
+    let html = ''
+    let str = this.phrase
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === ' '){
+          html += `<li class="space"> </li>`
+        } else{
+          html += `<li class="hide letter ${str[i]}">${str[i]}</li>`
+        }
+    }
+    listArea.innerHTML = html
+  };
+
+
+
+
  }
--- js/app.js
+++ js/app.js
@@ -1,12 +1,7 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-const logPhrase = (phrase) => {
-  console.log(`Phrase - phrase: `, phrase.phrase);
-};
-const game = new Game();
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
+ const game = new Game();
+ const randomPhrase = game.getRandomPhrase();
+ const phrase = new Phrase(randomPhrase.phrase);
+ phrase.addPhraseToDisplay();
